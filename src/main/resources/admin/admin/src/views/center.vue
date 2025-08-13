ada8a8403d9125ebf77f4e9da8c947daa0d7a59e
Good-Night-Unicorn <1685429765@qq.com>
2025-08-13 17:54:52 +0800

    班级综合

---
+++ src/main/resources/admin/admin/src/views/center.vue
@@ -0,0 +1,246 @@
+<template>
+  <div>
+    <el-form
+      class="detail-form-content"
+      ref="ruleForm"
+      :model="ruleForm"
+      label-width="80px"
+	  style="background: transparent;"
+    >  
+     <el-row>
+      <el-col :span="12">
+        <el-form-item   v-if="flag=='xuesheng'"  label="学号" prop="xuehao">
+          <el-input v-model="ruleForm.xuehao" readonly              placeholder="学号" clearable></el-input>
+        </el-form-item>
+      </el-col>
+      <el-col :span="12">
+        <el-form-item   v-if="flag=='xuesheng'"  label="学生姓名" prop="xueshengxingming">
+          <el-input v-model="ruleForm.xueshengxingming"               placeholder="学生姓名" clearable></el-input>
+        </el-form-item>
+      </el-col>
+      <el-col :span="12">
+        <el-form-item v-if="flag=='xuesheng'"  label="性别" prop="xingbie">
+          <el-select v-model="ruleForm.xingbie"  placeholder="请选择性别">
+            <el-option
+                v-for="(item,index) in xueshengxingbieOptions"
+                v-bind:key="index"
+                :label="item"
+                :value="item">
+            </el-option>
+          </el-select>
+        </el-form-item>
+      </el-col>
+      <el-col :span="24">  
+        <el-form-item v-if="flag=='xuesheng'" label="头像" prop="touxiang">
+          <file-upload
+          tip="点击上传头像"
+          action="file/upload"
+          :limit="3"
+          :multiple="true"
+          :fileUrls="ruleForm.touxiang?ruleForm.touxiang:''"
+          @change="xueshengtouxiangUploadChange"
+          ></file-upload>
+        </el-form-item>
+      </el-col>
+      <el-col :span="12">
+        <el-form-item   v-if="flag=='xuesheng'"  label="学生手机" prop="xueshengshouji">
+          <el-input v-model="ruleForm.xueshengshouji"               placeholder="学生手机" clearable></el-input>
+        </el-form-item>
+      </el-col>
+      <el-col :span="12">
+        <el-form-item v-if="flag=='xuesheng'"  label="班级" prop="banji">
+          <el-select v-model="ruleForm.banji"  placeholder="请选择班级">
+            <el-option
+                v-for="(item,index) in xueshengbanjiOptions"
+                v-bind:key="index"
+                :label="item"
+                :value="item">
+            </el-option>
+          </el-select>
+        </el-form-item>
+      </el-col>
+      <el-col :span="12">
+        <el-form-item v-if="flag=='xuesheng'"  label="教师工号" prop="jiaoshigonghao">
+          <el-select v-model="ruleForm.jiaoshigonghao"  placeholder="请选择教师工号">
+            <el-option
+                v-for="(item,index) in xueshengjiaoshigonghaoOptions"
+                v-bind:key="index"
+                :label="item"
+                :value="item">
+            </el-option>
+          </el-select>
+        </el-form-item>
+      </el-col>
+      <el-col :span="12">
+        <el-form-item   v-if="flag=='jiaoshi'"  label="教师工号" prop="jiaoshigonghao">
+          <el-input v-model="ruleForm.jiaoshigonghao" readonly              placeholder="教师工号" clearable></el-input>
+        </el-form-item>
+      </el-col>
+      <el-col :span="12">
+        <el-form-item   v-if="flag=='jiaoshi'"  label="教师姓名" prop="jiaoshixingming">
+          <el-input v-model="ruleForm.jiaoshixingming"               placeholder="教师姓名" clearable></el-input>
+        </el-form-item>
+      </el-col>
+      <el-col :span="12">
+        <el-form-item v-if="flag=='jiaoshi'"  label="性别" prop="xingbie">
+          <el-select v-model="ruleForm.xingbie"  placeholder="请选择性别">
+            <el-option
+                v-for="(item,index) in jiaoshixingbieOptions"
+                v-bind:key="index"
+                :label="item"
+                :value="item">
+            </el-option>
+          </el-select>
+        </el-form-item>
+      </el-col>
+      <el-col :span="24">  
+        <el-form-item v-if="flag=='jiaoshi'" label="照片" prop="zhaopian">
+          <file-upload
+          tip="点击上传照片"
+          action="file/upload"
+          :limit="3"
+          :multiple="true"
+          :fileUrls="ruleForm.zhaopian?ruleForm.zhaopian:''"
+          @change="jiaoshizhaopianUploadChange"
+          ></file-upload>
+        </el-form-item>
+      </el-col>
+      <el-col :span="12">
+        <el-form-item   v-if="flag=='jiaoshi'"  label="教师电话" prop="jiaoshidianhua">
+          <el-input v-model="ruleForm.jiaoshidianhua"               placeholder="教师电话" clearable></el-input>
+        </el-form-item>
+      </el-col>
+      <el-form-item v-if="flag=='users'" label="用户名" prop="username">
+        <el-input v-model="ruleForm.username" 
+        placeholder="用户名"></el-input>
+      </el-form-item>
+      <el-col :span="24">
+      <el-form-item>
+        <el-button type="primary" @click="onUpdateHandler">修 改</el-button>
+      </el-form-item>
+      </el-col>
+      </el-row>
+    </el-form>
+  </div>
+</template>
+<script>
+// 数字，邮件，手机，url，身份证校验
+import { isNumber,isIntNumer,isEmail,isMobile,isPhone,isURL,checkIdCard } from "@/utils/validate";
+
+export default {
+  data() {
+    return {
+      ruleForm: {},
+      flag: '',
+      usersFlag: false,
+      xueshengxingbieOptions: [],
+      xueshengbanjiOptions: [],
+      xueshengjiaoshigonghaoOptions: [],
+      jiaoshixingbieOptions: [],
+    };
+  },
+  mounted() {
+    var table = this.$storage.get("sessionTable");
+    this.flag = table;
+    this.$http({
+      url: `${this.$storage.get("sessionTable")}/session`,
+      method: "get"
+    }).then(({ data }) => {
+      if (data && data.code === 0) {
+        this.ruleForm = data.data;
+      } else {
+        this.$message.error(data.msg);
+      }
+    });
+    this.xueshengxingbieOptions = "男,女".split(',')
+    this.$http({
+      url: `option/banji/banji`,
+      method: "get"
+    }).then(({ data }) => {
+      if (data && data.code === 0) {
+        this.xueshengbanjiOptions = data.data;
+      } else {
+        this.$message.error(data.msg);
+      }
+    });
+    this.$http({
+      url: `option/jiaoshi/jiaoshigonghao`,
+      method: "get"
+    }).then(({ data }) => {
+      if (data && data.code === 0) {
+        this.xueshengjiaoshigonghaoOptions = data.data;
+      } else {
+        this.$message.error(data.msg);
+      }
+    });
+    this.jiaoshixingbieOptions = "男,女".split(',')
+  },
+  methods: {
+    xueshengtouxiangUploadChange(fileUrls) {
+        this.ruleForm.touxiang = fileUrls;
+    },
+    jiaoshizhaopianUploadChange(fileUrls) {
+        this.ruleForm.zhaopian = fileUrls;
+    },
+    onUpdateHandler() {
+      if((!this.ruleForm.xuehao)&& 'xuesheng'==this.flag){
+        this.$message.error('学号不能为空');
+        return
+      }
+      if((!this.ruleForm.mima)&& 'xuesheng'==this.flag){
+        this.$message.error('密码不能为空');
+        return
+      }
+        if(this.ruleForm.touxiang!=null) {
+                this.ruleForm.touxiang = this.ruleForm.touxiang.replace(new RegExp(this.$base.url,"g"),"");
+        }
+      if( 'xuesheng' ==this.flag && this.ruleForm.xueshengshouji&&(!isMobile(this.ruleForm.xueshengshouji))){
+        this.$message.error(`学生手机应输入手机格式`);
+        return
+      }
+      if((!this.ruleForm.jiaoshigonghao)&& 'jiaoshi'==this.flag){
+        this.$message.error('教师工号不能为空');
+        return
+      }
+      if((!this.ruleForm.mima)&& 'jiaoshi'==this.flag){
+        this.$message.error('密码不能为空');
+        return
+      }
+      if((!this.ruleForm.jiaoshixingming)&& 'jiaoshi'==this.flag){
+        this.$message.error('教师姓名不能为空');
+        return
+      }
+        if(this.ruleForm.zhaopian!=null) {
+                this.ruleForm.zhaopian = this.ruleForm.zhaopian.replace(new RegExp(this.$base.url,"g"),"");
+        }
+      if( 'jiaoshi' ==this.flag && this.ruleForm.jiaoshidianhua&&(!isMobile(this.ruleForm.jiaoshidianhua))){
+        this.$message.error(`教师电话应输入手机格式`);
+        return
+      }
+      if('users'==this.flag && this.ruleForm.username.trim().length<1) {
+	this.$message.error(`用户名不能为空`);
+        return	
+      }
+      this.$http({
+        url: `${this.$storage.get("sessionTable")}/update`,
+        method: "post",
+        data: this.ruleForm
+      }).then(({ data }) => {
+        if (data && data.code === 0) {
+          this.$message({
+            message: "修改信息成功",
+            type: "success",
+            duration: 1500,
+            onClose: () => {
+            }
+          });
+        } else {
+          this.$message.error(data.msg);
+        }
+      });
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+</style>
